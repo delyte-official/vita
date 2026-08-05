1621727ea7d64490b91cc24e0906275f3ecb5648
Delyte <150934645+delyte-official@users.noreply.github.com>
2026-08-05 21:46:08 +0200

    fix: pretty printer, dot token, else branch parsing
    Changed the pretty printer from a normal visitor to a debug printer, with special indentation for special cases like statements with if conditions
    Fixed the lexing of the dot, which was considered an invalid literal
    Fixed the parsing of the else branch in a if statement, for the allow block.

--- lumen/src/parser/ast/expr.rs
+++ lumen/src/parser/ast/expr.rs
@@ -1,12 +1,12 @@
+use std::fmt;
+
 use super::BinaryOp;
 
-#[derive(Debug)]
 pub enum LiteralPart {
     Text(String),
     Expr(Expr),
 }
 
-#[derive(Debug)]
 pub enum Expr {
     Literal(String),
     TemplateLiteral(Vec<LiteralPart>),
@@ -22,5 +22,29 @@ pub enum Expr {
 impl Expr {
     pub fn binary(op: BinaryOp, left: Expr, right: Expr) -> Expr {
         Expr::Binary { op, left: Box::new(left), right: Box::new(right) }
+    }
+}
+
+impl fmt::Debug for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Expr::Literal(s) => write!(f, "{}", s),
+            Expr::TemplateLiteral(parts) => {
+                let parts_str = parts
+                    .iter()
+                    .map(|part| match part {
+                        LiteralPart::Text(s) => format!("{}", s),
+                        LiteralPart::Expr(e) => format!("{{{:#?}}}", e),
+                    })
+                    .collect::<Vec<String>>()
+                    .join("");
+                write!(f, "{}", parts_str)
+            }
+            Expr::Binary { op, left, right } => {
+                write!(f, "{:#?} {:#?} {:#?}", left, op, right)
+            }
+            Expr::Var(name) => write!(f, "{}", name),
+            Expr::FuncCall { name } => write!(f, "{}()", name),
+        }
     }
 }
--- lumen/src/parser/ast/function.rs
+++ lumen/src/parser/ast/function.rs
@@ -1,8 +1,24 @@
+use std::fmt;
+
 use crate::parser::Stmt;
 
-#[derive(Debug)]
 pub struct Function {
     pub name: String,
     pub body: Vec<Stmt>,
     pub return_type: Option<String>,
+}
+
+impl fmt::Debug for Function {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let body_str = self
+            .body
+            .iter()
+            .map(|stmt| format!("{:#?}", stmt))
+            .collect::<Vec<String>>()
+            .join("\n");
+        write!(f, "func {}(){} {{\n{}\n}}", self.name, match &self.return_type {
+            Some(t) => format!(": {}", t),
+            None => String::new(),
+        }, body_str)
+    }
 }
